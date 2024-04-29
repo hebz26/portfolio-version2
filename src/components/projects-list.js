6f55e6b78104be1a61f1d4f18545758ece59c7c4
Heba Sayed <hebasayed1603@gmail.com>
2024-04-28 23:14:31 -0400

    list of projects

---
+++ src/components/projects-list.js
@@ -0,0 +1,27 @@
+const projects = [
+  {
+    title: "Google Event",
+    description:
+      "Built website for the Detroit International Women's Day Summit 2024",
+    imageUrl: "https://via.placeholder.com/300",
+    demoLink: "https://www.iwdsummit.com/",
+  },
+  {
+    title: "Half Project 2",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl: "https://via.placeholder.com/300",
+    codeLink: "https://example.com/code2",
+    demoLink: "https://example.com/demo2",
+  },
+  {
+    title: "Half Project 3",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageUrl: "https://via.placeholder.com/300",
+    codeLink: "https://example.com/code3",
+    demoLink: "https://example.com/demo3",
+  },
+];
+
+export default projects;
