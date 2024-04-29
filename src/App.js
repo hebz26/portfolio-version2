f30724bee0847aeb00776632eb810e5aa64554f5
Heba Sayed <hebasayed1603@gmail.com>
2024-04-29 01:33:08 -0400

    added sections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Work from "./components/Work";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
+      <About />
+      <Work />
     </div>
   );
 }
